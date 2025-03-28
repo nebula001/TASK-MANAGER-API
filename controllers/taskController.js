df338a987314604796bb1df57c8eb8bb6da57cf4
Aameer <officialaameer001@gmail.com>
2025-03-29 04:24:54 +0530

    Implemented custrom error handler with custom error class

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,18 +1,24 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
+const {
+  createCustomError,
+  customAPIError,
+} = require("../errors/custom-errors");
 
 const getTasks = asyncWrapper(async (req, res) => {
   const allTasks = await Task.find();
   return res.status(200).json({ success: true, msg: "Get All Task", allTasks });
 });
 
-const getTaskById = asyncWrapper(async (req, res) => {
+const getTaskById = asyncWrapper(async (req, res, next) => {
   const { id: taskId } = req.params;
   const singleTask = await Task.findById(taskId);
   if (!singleTask) {
-    return res
-      .status(400)
-      .json({ success: true, msg: `No task with id of ${taskId} available` });
+    const checkError = createCustomError(
+      `No task with id of ${taskId} available`,
+      400
+    );
+    return next(checkError);
   }
   return res
     .status(200)
@@ -27,30 +33,31 @@ const createTask = asyncWrapper(async (req, res) => {
     .json({ success: true, msg: "Create new Task", newTask });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskId } = req.params;
   const newTask = await Task.findByIdAndUpdate(taskId, req.body, {
     runValidators: true,
     new: true,
   });
   if (!newTask) {
-    return res
-      .status(400)
-      .json({ success: true, msg: `No task with id of ${taskId} available` });
+    const checkError = createCustomError(
+      `No task with id of ${taskId} available`,
+      400
+    );
+    return next(checkError);
   }
   return res.status(201).json({ success: true, msg: "Update Task", newTask });
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskId } = req.params;
   const deletedTask = await Task.findByIdAndDelete(taskId);
   if (!deletedTask) {
-    return res
-      .status(400)
-      .json({
-        success: true,
-        msg: `No task with id of ${taskId} available to delete`,
-      });
+    const checkError = createCustomError(
+      `No task with id of ${taskId} available`,
+      400
+    );
+    return next(checkError);
   }
   return res
     .status(200)
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,10 @@
+const { customAPIError } = require("../errors/custom-errors");
+
 const errormiddleware = (err, req, res, next) => {
+  if (err instanceof customAPIError) {
+    return res.status(err.statusCode).json({ success: true, msg: err.message });
+  }
+
   return res
     .status(500)
     .json({ success: false, msg: "Something went wrong", err });
